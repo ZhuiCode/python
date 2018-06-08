5d75a473e278f951531d342715c44f0a048b1d02
ZhuiCode <zhui890530@outlook.com>
2018-06-08 18:50:34 +0800

    添加问题文件及python测试代码文件

---
+++ generator.py
@@ -0,0 +1,22 @@
+#def gen_generator():
+#   yield 1
+
+#def gen_value():
+#    return 1
+def fib():
+    a, b = 1, 1
+    while True:
+        yield a
+        a, b = b, a+b 
+
+if __name__ == '__main__':
+    ret = fib()
+    print (next(ret))    #<generator object gen_generator at 0x02645648> <type 'generator'>
+    print (next(ret))
+    print (next(ret))
+    print (next(ret))
+    print (next(ret))
+    print (next(ret))
+
+    #ret = gen_value()
+    #print (ret, type(ret))   # 1 <type 'int'>
